4a0679be77bd898c8934e6def1c6a18d21abc3f0
Roman Nazarevych <rnaz@softserveinc.com>
2017-05-14 02:23:30 +0300

    refactored icons added transport type property to model added color property

--- app/src/main/java/org/rnaz/lvivpubtrans/controller/RoutesAdapter.java
+++ app/src/main/java/org/rnaz/lvivpubtrans/controller/RoutesAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.rnaz.lvivpubtrans.R;
@@ -33,7 +34,9 @@ public class RoutesAdapter extends RecyclerView.Adapter<RoutesAdapter.RouteVH> {
 
     @Override
     public void onBindViewHolder(RouteVH holder, int position) {
-        holder.routeName.setText(models.get(position).getName());
+        IRouteModel routeModel = models.get(position);
+        holder.routeName.setText(routeModel.getName());
+        holder.setIcon(routeModel.getType());
     }
 
     @Override
@@ -48,16 +51,32 @@ public class RoutesAdapter extends RecyclerView.Adapter<RoutesAdapter.RouteVH> {
 
     class RouteVH extends RecyclerView.ViewHolder{
         TextView routeName;
+        ImageView routeIcon;
 
         RouteVH(final View itemView) {
             super(itemView);
             routeName = (TextView) itemView.findViewById(R.id.route_name);
+            routeIcon = (ImageView) itemView.findViewById(R.id.route_icon);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     onItemSelected.onSelected(models.get(getAdapterPosition()));
                 }
             });
+        }
+
+        void setIcon (@RouteModel.TransportType int transportType){
+            switch (transportType) {
+                case RouteModel.BUS:
+                    routeIcon.setImageResource(R.drawable.bus);
+                    break;
+                case RouteModel.TRAM:
+                    routeIcon.setImageResource(R.drawable.tram);
+                    break;
+                case RouteModel.TROLLEYBUS:
+                    routeIcon.setImageResource(R.drawable.trolleybus);
+                    break;
+            }
         }
     }
 }
